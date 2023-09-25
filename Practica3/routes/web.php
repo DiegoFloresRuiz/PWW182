ab89a59dbb717c53f56b43e6bd658011580645d2
DiegoFloresRuiz <133287417+DiegoFloresRuiz@users.noreply.github.com>
2023-09-25 17:17:16 -0600

    Abance1

--- Practica3/routes/web.php
+++ Practica3/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::view('/','welcome');
+Route::view('/formulario','formulario');
+Route::view('/recuerdos','recuerdos');
